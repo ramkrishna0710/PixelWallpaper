ea06f11384a6a522eb7e856d0793e29a6a32d9e2
ramkrishna0710 <113228237+ramkrishna0710@users.noreply.github.com>
2025-02-03 15:53:44 +0530

    update statusbar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import PixelWallpapers from './src/screens/PixelWallpapers';
@@ -8,6 +8,7 @@ const queryClinet = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClinet}>
+      <StatusBar translucent backgroundColor={'transparent'}/>
       <View style={styles.container}>
         <PixelWallpapers/>
       </View>
